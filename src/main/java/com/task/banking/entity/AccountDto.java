84a79f633b2009b70b17666bc2d2440f6209fa7d
smarcell91 <smarcell91@gmail.com>
2022-02-22 20:57:18 +0100

    account DTO

---
+++ src/main/java/com/task/banking/entity/AccountDto.java
@@ -0,0 +1,11 @@
+package com.task.banking.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountDto {
+    private String name;
+    private String password;
+}
